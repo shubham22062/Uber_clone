10f1d5874b6597d239e1adb65a5b9e767e9b6b45
Shubham Awasthi <shubhamawasthi021@gmail.com>
2025-10-09 22:05:56 +0530

    ten

---
+++ Backend/src/services/map.service.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+import captainModel from '../models/captain.model'
+
+export const getAddressCoridnate = async (address)=>{
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = 
+}
