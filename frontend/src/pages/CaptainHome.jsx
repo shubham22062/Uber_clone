913079a19d52c578d83e0c4603a59d944e4d9524
Shubham Awasthi <shubhamawasthi021@gmail.com>
2025-10-20 21:54:02 +0530

    CaptainHome

---
+++ frontend/src/pages/CaptainHome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UserContext from "../context/UserContext";
+import { useNavigate } from "react-router-dom";\
+
+
+export  const CaptainHome  = async(){
+
+}
+
+export default CaptainHome;
