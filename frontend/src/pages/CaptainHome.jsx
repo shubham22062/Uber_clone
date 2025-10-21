3a7e35dbcbaec047b2e52bf66a07dedddce67aa4
Shubham Awasthi <shubhamawasthi021@gmail.com>
2025-10-21 23:55:52 +0530

    home

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserContext from "../context/UserContext";
 import { useNavigate } from "react-router-dom";\
 
 
-export  const CaptainHome  = async(){
+export  const CaptainHome  = ()=>{
+ const [ridePopupPanel, setRidePopupPanel] = usseState(false)
+ const [confirmRidePopupPanel, setConfirmRidePanel] = useState(false)
+ const ridePopupPanelRef = useRef(null)
+ const confirmRidePopupPanelRef = useRef(null)
+ const [ride , setRide] = useState(null)
 
+ const {socket}= useContext(SocketContext)
+ const {captain}  = useContext(CaptainDataContext)
 }
 
 export default CaptainHome;
